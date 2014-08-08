28d8fc7706784e6f508aa66a9390cd38f4600714
Yule <paul.brennan@ahc.uk.com>
2014-08-08 16:47:20 +0100

    test for empty string in all encoding

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,11 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    encoding = str.encoding
+    
+    #If its dummy encoding, force UTF8
+    return str.to_s.force_encoding(Encoding::UTF_8) =~ /^.+$/ if encoding.dummy?
+
+    #enocde the regex as the same encoding as the string
+    str =~ Regexp.new('^.+$'.encode(encoding))
   end
 end
--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -4,8 +4,28 @@ require_relative '../lib/regexp_m17n'
 
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
-    Encoding.list.each do |enc|
-      assert(RegexpM17N.non_empty?('.'.encode(enc)))
+    Encoding.list.reject(&:dummy?).each do |enc|
+      assert(RegexpM17N.non_empty?('.'.encode(enc)), "Failed with #{enc}")
     end
   end
+
+  def test_empty_string
+    Encoding.list.reject(&:dummy?).each do |enc|
+      assert(!RegexpM17N.non_empty?(''.encode(enc)), "Failed with #{enc}")
+    end
+  end
+  
+  
+  def test_non_empty_string_dummy
+    Encoding.list.select(&:dummy?).each do |enc|
+      assert(RegexpM17N.non_empty?('.'.force_encoding(enc)), "Failed with #{enc}")
+    end
+  end
+  
+  def test_empty_string_dummy
+    Encoding.list.select(&:dummy?).each do |enc|
+      assert(!RegexpM17N.non_empty?(''.force_encoding(enc)), "Failed with #{enc}")
+    end
+  end
+
 end
